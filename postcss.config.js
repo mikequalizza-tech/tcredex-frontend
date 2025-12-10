3098c8e9964182180c1bf203281ac60c09d494ff
Michael Qualizza <mikequalizza@gmail.com>
2025-12-10 16:28:48 -0600

    Fix Tailwind v4 PostCSS plugin config

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,9 @@
 // postcss.config.js
 module.exports = {
   plugins: {
-    // force require from CJS entrypoint to bypass ESM export issue
-    'postcss-import': {},
-    'tailwindcss': { config: './tailwind.config.ts' },
-    'autoprefixer': {},
-  }
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
 }
 
 

84e0b1340e8b195ba3f36ee85353ba381e9883ee
Michael Qualizza <mikequalizza@gmail.com>
2025-12-11 12:22:36 -0600

    fix: dark background and add brand assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 // app/layout.tsx
 
-
 import "./css/style.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -15,7 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-gray-950 text-gray-200 antialiased`}>{children}</body>
     </html>
   );
 }

75361193ed8621f72185799c737a712074fef90e
Michael Qualizza <mikequalizza@gmail.com>
2025-12-10 20:53:47 -0600

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,39 +1,14 @@
 // tailwind.config.ts
 
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-        nacelle: ['Nacelle', 'sans-serif'],
-      },
-      keyframes: {
-        shine: {
-          "0%": { top: "0", transform: "scaleY(5)", opacity: "0" },
-          "10%": { opacity: "0.8" },
-          "20%": { top: "100%", transform: "scaleY(10)", opacity: "0" },
-          "100%": { top: "100%", transform: "scaleY(1)", opacity: "0" },
-        },
-        gradient: {
-          to: { backgroundPosition: "200% center" },
-        },
-      },
-      animation: {
-        shine: "shine 5s ease-in-out 500ms infinite",
-        gradient: "gradient 6s linear infinite",
-      },
-    },
+    extend: {},
   },
-  plugins: [
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/typography"),
-  ],
-};
-
-export default config;
+  plugins: [],
+}
